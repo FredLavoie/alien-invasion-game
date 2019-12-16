0102879a71529ee057be5203ab97b78e7a69add1
FredLavoie <flavoie3@gmail.com>
2019-12-15 19:01:26 -0500

    Add alien class and refactor

--- settings.py
+++ settings.py
@@ -6,6 +6,8 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        
+        # Ship settings
         self.ship_speed = 10
 
         # Bullet settings
@@ -13,4 +15,10 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 6
+        self.bullets_allowed = 7
+
+        # Alien settings
+        self.alien_speed = 6.0
+        self.fleet_drop_speed = 15
+        #fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
